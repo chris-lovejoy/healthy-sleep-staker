54791b839ae9ec2d499de73b5da9fe8503280eaf
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-05-09 15:09:26 +0100

    add chivo fontsource and colour mode

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from './theme';
+import "@fontsource/chivo/400.css";
+import "@fontsource/chivo/700.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
         <App />
     </ChakraProvider>
   </React.StrictMode>
